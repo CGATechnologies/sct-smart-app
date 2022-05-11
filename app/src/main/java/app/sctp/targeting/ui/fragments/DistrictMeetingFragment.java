75617b14a245421d188eda677a8fb1f8fe82d6c6
kamfosica <83751365+kamfosica@users.noreply.github.com>
2022-05-11 16:38:13 +0200

    Update cards with step numbers

--- app/src/main/java/app/sctp/targeting/ui/fragments/DistrictMeetingFragment.java
+++ app/src/main/java/app/sctp/targeting/ui/fragments/DistrictMeetingFragment.java
@@ -2,6 +2,8 @@ package app.sctp.targeting.ui.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
 
+import app.sctp.R;
 import app.sctp.core.ui.BindableFragment;
 import app.sctp.databinding.FragmentTargetingDistrictMeetingBinding;
 
@@ -16,6 +19,18 @@ public class DistrictMeetingFragment extends BindableFragment {
 
     private FragmentTargetingDistrictMeetingBinding binding;
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.district_meeting_options, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
